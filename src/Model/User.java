d55cbee159886f62f5dd1ace358896f5d1c531e2
Kamburugamuwa K.L.A.P.T.J it17092548 <it17092548@my.sliit.lk>
2019-03-16 18:38:14 +0530

    added all

--- src/Model/User.java
+++ src/Model/User.java
@@ -10,45 +10,53 @@ public class User {
 	public String password;
 	
 	public User(){
-		
 	}
 	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastname() {
 		return lastname;
 	}
+	
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public int getPhone() {
 		return phone;
 	}
+	
 	public void setPhone(int phone) {
 		this.phone = phone;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-
 }
---
+++ src/Services/Authentication.java
@@ -0,0 +1,15 @@
+package Services;
+
+public class Authentication {
+	
+	
+	private static Authentication singleton = new Authentication();
+
+	
+	private Authentication() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
